e26075ef4e1617a6b84393bbddf1cd16ca238af7
tc <tc@mcdex.io>
2021-09-22 21:38:38 +0800

    optimize 1000

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -161,7 +161,7 @@ module.exports = {
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 1
+                runs: 1000
             }
         }
     },
